ab2c5a581779edbd64dcf9a7737b67ebb59b214b
Viktoriia <pecourses>
2024-02-04 18:29:47 +0200

    Add create user

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,4 +1,21 @@
-module.exports.createUser = async (req, res, next) => {};
+const createHttpError = require('http-errors');
+const { User } = require('../model');
+
+module.exports.createUser = async (req, res, next) => {
+  const { body } = req;
+
+  try {
+    const createdUser = await User.create(body);
+
+    if (!createdUser) {
+      return next(createHttpError(400, 'Bad request'));
+    }
+
+    res.status(201).send({ data: createdUser });
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports.getUsers = async (req, res, next) => {};
 
